4965bd0d460e186dedb269386ab36d402e4f8697
Dillon R. Gardner <dillon.gardner@enernoc.com>
2016-03-22 17:57:51 -0400

    Improved documentation of PCA

--- 3-1-PCA/example.R
+++ 3-1-PCA/example.R
@@ -15,25 +15,26 @@ setwd("~/src/ArchConfRML/3-1-PCA/data/")
 
 # Read and format .csv files into data.frames
 readFile <- function(file){
-  df <- read.csv(file, stringsAsFactors = F)
-  df$DATE <- as.Date(df$DATE)
-  df$TIME <- as.POSIXct(df$TIME, tz="UTC")
-  return(df)
+  timeseries_df <- read.csv(file, stringsAsFactors = F)
+  timeseries_df$DATE <- as.Date(timeseries_df$DATE)
+  timeseries_df$TIME <- as.POSIXct(timeseries_df$TIME, tz="UTC")
+  return(timeseries_df)
 }
 
 # Format data into matrix necessary for PCA analysis
-formatMatrix <- function(df){
-  df <- df %>% select(HOUR, DATE, DEMAND)
-  df <- df %>% spread(DATE, DEMAND)
-  row.names(df) <- df$HOUR
-  df <- df %>% select(-HOUR)
-  df <- t(df)
-  return(df)
+pivotToMatrix <- function(timeseriesDF){
+  timeseriesDF <- timeseriesDF %>% select(HOUR, DATE, DEMAND)
+  timeseriesMatrix <- timeseriesDF %>% spread(DATE, DEMAND) # Pivot so each column is one day
+  row.names(timeseriesMatrix) <- timeseriesMatrix$HOUR
+  timeseriesMatrix <- timeseriesMatrix %>% select(-HOUR) #Drop unecessary hour column
+  timeseriesMatrix <- t(timeseriesMatrix) # Transpose so each row is a day
+  return(timeseriesMatrix)
 }  
 
 # Create time series plot of electricy use
-plotDygraph <- function(df){
-  myXTS <- xts(x=df$DEMAND, order.by=df$TIME, tzone="UTC")
+plotDygraph <- function(timeseriesDF){
+  # Create xts object of data which is needed for dygraph package
+  myXTS <- xts(x=timeseriesDF$DEMAND, order.by=timeseriesDF$TIME, tzone="UTC")
   colnames(myXTS) <- "DEMAND"
   dygraph(myXTS, main="Electricy Demand") %>%
     dyRangeSelector(dateWindow=c("2014/10/3", "2014/10/17")) %>%
@@ -41,10 +42,10 @@ plotDygraph <- function(df){
 }
 
 # Plot each day according to the first two principle components
-plotPCA <- function(res){
-  plotData <- data.frame(DATE=rownames(res$x),
-                    PC1=res$x[,1],
-                    PC2=res$x[,2])
+plotPCA <- function(PCAresults){
+  plotData <- data.frame(DATE=rownames(PCAresults$x),
+                    PC1=PCAresults$x[,1],
+                    PC2=PCAresults$x[,2])
   p <- plot_ly(plotData, x = -1*PC1, y = -1*PC2, 
          text = paste("Date: ", DATE), # Label Points
          mode = "markers", # Plot points
@@ -57,8 +58,8 @@ plotPCA <- function(res){
 
 
 # Plot the shape of the first two principle components
-plotComponents <- function(res){
-  plotData <- as.data.frame(-1*res$rotation[,1:2]) %>%
+plotComponents <- function(PCAresults){
+  plotData <- as.data.frame(-1*PCAresults$rotation[,1:2]) %>%
     mutate(Hour=0:23) %>%
     gather("Principle_Component", "Value", -Hour)
   ggplot(plotData, aes(x=Hour, y=Value)) + 
@@ -66,15 +67,15 @@ plotComponents <- function(res){
 }
 
 # Change file to explore other sites
-siteData <- readFile("agricultural.csv")
+siteDF <- readFile("agricultural.csv")
 
 # This line is all it takes to calculate PCA in R!
-res <- prcomp(formatMatrix(siteData), scale=F, center=F) # Calcualte PCA
+PCAresults <- prcomp(pivotToMatrix(siteDF), scale=F, center=F) # Calcualte PCA
 
 # Now we make some pretty plots
-plotDygraph(siteData)
-plotComponents(res)
-plotPCA(res)
+plotDygraph(siteDF)
+plotComponents(PCAresults)
+plotPCA(PCAresults)
 
 
 
